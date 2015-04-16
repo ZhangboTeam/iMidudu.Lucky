9407d7b8e234dfa5e1cfef81a02eed7c24c9fa66
wj620645 <872689338@qq.com>
2015-04-16 23:46:31 +0800

    login111

---
+++ 代码/iMidudu.Lucky/iMidudu.Lucky.Web/Admin/js/Login.js
@@ -0,0 +1,34 @@
+﻿var AuthcationServiceUrl = "/ajax/AuthcationService.asmx/";
+function Login(userName, password, successHandler, errHandler) {
+    callAjaxWebService(AuthcationServiceUrl, "Login", { UserName: userName, Password: password }, successHandler, errHandler);
+}
+function callAjaxWebService(serviceUrl, methodName, param, successHandler, errHandler) {
+    window.status = "";
+    $.ajax({
+        type: "POST",
+        contentType: "application/json",
+        url: serviceUrl + methodName,
+        data: JSON.stringify(param),
+        dataType: 'json',
+        success: function (result) {
+            if (successHandler != null) {
+                successHandler(result.d);
+            } else {
+
+                $.gritter.add({
+                    title: 'MQE',
+                    text: 'Save Data Success!<br/>',
+                    class_name: 'gritter-success gritter-light'
+                });
+                window.status = "OK:" + methodName + ",param: " + JSON.stringify(param);
+            }
+        },
+        error: function (err) {
+            if (errHandler != null) {
+                errHandler(err);
+            } else {
+                window.status = serviceUrl + methodName + "," + err.responseText;
+            }
+        }
+    });
+}
